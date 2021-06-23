15004737925daf84b7f0bc0c402f62e9f0bf4897
Nilesh Patva <nileshpatva40@gmail.com>
2021-06-23 12:48:48 +0000

    stocks question - Groupon

---
+++ practice/stocks.js
@@ -0,0 +1,31 @@
+/**
+ * Groupon interview question
+ * [100, 180, 260, 380, 40, 550, 650]
+ */
+
+function maxProfit(stocks) {
+  let buy = 0;
+  let sell = 1;
+  let max = 0;
+
+  let idx = [];
+
+  while (sell < stocks.length) {
+    let profit = stocks[sell] - stocks[buy];
+    if (profit > 0) {
+      if (idx.length > 0 && idx[idx.length - 1][1] === buy) {
+        idx[idx.length - 1][1] = sell;
+      } else {
+        idx.push([buy, sell]);
+      }
+      max += profit;
+      buy++;
+    } else {
+      buy = sell;
+    }
+    sell++;
+  }
+  console.log({ max, idx });
+}
+
+maxProfit([100, 180, 260, 380, 40, 550, 650, 30, 200, 100, 400, 600]);
